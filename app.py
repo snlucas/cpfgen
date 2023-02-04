9fe1b47557464ec3b25eef811385d5b44631f74f
Lucas Nogueira <lucas.lambda.101@gmail.com>
2023-02-04 15:29:52 -0300

    Improve time to run removing images

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ def get_cpf_value():
 
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
+    options.add_argument('--blink-settings=imagesEnabled=false')
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
     # Find the "GERAR CPF" button and click it
-    gerar_cpf_button = driver.find_element(By.CSS_SELECTOR, "#bt_gerar_cpf")
-    gerar_cpf_button.click()
+    driver.find_element(By.ID, "bt_gerar_cpf").click()
 
     # Find the CPF input value
     cpf_input = driver.find_element(By.ID, "texto_cpf")
@@ -21,14 +21,11 @@ def get_cpf_value():
 
     while cpf_value == "Gerando...":
         cpf_value = cpf_input.get_attribute('innerText')
-    
-    pyperclip.copy(cpf_value)
-    print(cpf_value)
 
     driver.quit()
-
+    pyperclip.copy(cpf_value)
+    print(cpf_value)
 if __name__ == '__main__':
     p = multiprocessing.Process(target=get_cpf_value)
     p.start()
     p.join()
-
